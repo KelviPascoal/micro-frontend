6d054cacd7c798e56ee7cbef68e5fa662625bca0
kelvi <kelvi_ugo@hotmail.com>
2022-05-28 18:15:28 -0300

    feat: create the react structure

---
+++ packages/home/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+export function Home() {
+    return (
+        <>
+                <h1 className="display-3">Olá Module Federation</h1>
+                <hr className="my-2" />
+                <p>Este componente é de outro App!</p>
+                <p className="lead">
+                    <Button color="primary">Botão</Button>
+                </p>
+
+        </>
+    )
+}
