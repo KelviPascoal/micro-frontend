0d750bcc3733acfb8652dc2b96ef7a9202361b59
kelvi <kelvi_ugo@hotmail.com>
2022-05-28 19:43:36 -0300

    feat: react project structure for contact

---
+++ packages/contact/src/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './contact.css'
+
+export function Contact() {
+    return (
+        <>
+            <h1>Página de contato</h1>
+            <p>Esta é uma simples página de contato</p>
+        </>
+    )
+}
